ca87abeffba6040882c71b4eec9fa21365c942c7
Kassem-Fahed-Dev <kassemfahed2003@gmail.com>
2025-02-08 23:16:19 -0800

    set up

---
+++ server.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: `./config.env` });
+const app = require('./app');
+const db = process.env.DATABASE.replace('<PASSWORD>',process.env.password)
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log('done'))
+  .catch((err) => console.log("not done"));
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log('the server is running');
+});
